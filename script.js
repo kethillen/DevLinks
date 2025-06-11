7d5f82d82c7acafbd9a159e030ed13af4e41e8a9
Kethillen <kethillen123barboz@gmail.com>
2025-06-11 10:21:16 -0300

    alteração foto de perfil no e social links no html, e alteralçao na foto de perfil no JS modo claro e escuro

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "././assets/meuPerfilClaro.jpg")
+    img.setAttribute("src", "./assets/perfilClaro.jpg")
   } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/meuPerfilEscuro.jpg")
+    img.setAttribute("src", "./assets/perfilEscuro.jpg")
   }
 }
